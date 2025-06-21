a790e74dc0c397fccdf5c3691561c6f4c3fa701b
Rahaf-Sabra <60314078@udst.edu.qa>
2025-06-21 04:11:19 +0300

    Some classes needed

--- src/project/Appointment.java
+++ src/project/Appointment.java
@@ -1,10 +1,28 @@
 package project;
 
-public class Appointment {
-	public String name;
+import java.util.*;
 
+<<<<<<< Updated upstream
 	public Appointment(String name) {
 		super();
 		this.name = name;
 	}
+=======
+public class Appointment {
+	private VehicleOwner vehicleOwner;
+    private GarageProfile garage;
+    private Date appointmentDateTime;
+    private Status status;
+    private Map<Service, Double> services;
+    
+ // Private constructor used by the builder
+    private Appointment(AppointmentBuilder builder) {
+        this.vehicleOwner = builder.vehicleOwner;
+        this.garage = builder.garage;
+        this.appointmentDateTime = builder.appointmentDateTime;
+        this.status = builder.status;
+        this.services = builder.services;
+    }
+
+>>>>>>> Stashed changes
 }
