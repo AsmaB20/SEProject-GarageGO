c93861379d2e232abd98a4fa4f9d3fd965243517
Rahaf-Sabra <60314078@udst.edu.qa>
2025-06-21 04:26:09 +0300

    Update and try something

--- src/project/Appointment.java
+++ src/project/Appointment.java
@@ -2,12 +2,6 @@ package project;
 
 import java.util.*;
 
-<<<<<<< Updated upstream
-	public Appointment(String name) {
-		super();
-		this.name = name;
-	}
-=======
 public class Appointment {
 	private VehicleOwner vehicleOwner;
     private GarageProfile garage;
@@ -24,5 +18,70 @@ public class Appointment {
         this.services = builder.services;
     }
 
->>>>>>> Stashed changes
+	public VehicleOwner getVehicleOwner() {
+		return vehicleOwner;
+	}
+
+	public void setVehicleOwner(VehicleOwner vehicleOwner) {
+		this.vehicleOwner = vehicleOwner;
+	}
+
+	public GarageProfile getGarage() {
+		return garage;
+	}
+
+	public void setGarage(GarageProfile garage) {
+		this.garage = garage;
+	}
+
+	public Date getAppointmentDateTime() {
+		return appointmentDateTime;
+	}
+
+	public void setAppointmentDateTime(Date appointmentDateTime) {
+		this.appointmentDateTime = appointmentDateTime;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public Map<Service, Double> getServices() {
+		return services;
+	}
+
+	public void setServices(Map<Service, Double> services) {
+		this.services = services;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(appointmentDateTime, garage, services, status, vehicleOwner);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Appointment other = (Appointment) obj;
+		return Objects.equals(appointmentDateTime, other.appointmentDateTime) && Objects.equals(garage, other.garage)
+				&& Objects.equals(services, other.services) && status == other.status
+				&& Objects.equals(vehicleOwner, other.vehicleOwner);
+	}
+
+	@Override
+	public String toString() {
+		return "Appointment [vehicleOwner=" + vehicleOwner + ", garage=" + garage + ", appointmentDateTime="
+				+ appointmentDateTime + ", status=" + status + ", services=" + services + "]";
+	}
+    
+
 }
