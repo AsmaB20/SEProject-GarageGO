804bb6ca03e060b67681785396740ad528411201
Rahaf-Sabra <60314078@udst.edu.qa>
2025-06-21 13:49:14 +0300

    create some related classes

--- src/project/VehicleOwner.java
+++ src/project/VehicleOwner.java
@@ -1,5 +1,48 @@
 package project;
 
-public class VehicleOwner {
-	
+import java.util.*;
+
+public class VehicleOwner extends User {
+	private String phoneNumber;
+	private Set<Appointment> appointments;
+	private VehicleType vehicleType;
+
+	public VehicleOwner(String username, String email, String phoneNumber, VehicleType vehicleType) {
+		super(username, email);
+		this.phoneNumber = phoneNumber;
+		this.vehicleType = vehicleType;
+		this.appointments = new HashSet<>();
+
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Set<Appointment> getAppointments() {
+		return appointments;
+	}
+
+	public void setAppointments(Set<Appointment> appointments) {
+		this.appointments = appointments;
+	}
+
+	public VehicleType getVehicleType() {
+		return vehicleType;
+	}
+
+	public void setVehicleType(VehicleType vehicleType) {
+		this.vehicleType = vehicleType;
+	}
+
+	@Override
+	public String toString() {
+		return "VehicleOwner [phoneNumber=" + phoneNumber + ", appointments=" + appointments + ", vehicleType="
+				+ vehicleType + "]";
+	}
+
 }
