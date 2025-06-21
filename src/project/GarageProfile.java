ba6e4a68b3ba5dfbca3d582fc4ac2a832c43ec4d
Rahaf-Sabra <60314078@udst.edu.qa>
2025-06-21 13:47:37 +0300

    create the garage profile

--- src/project/GarageProfile.java
+++ src/project/GarageProfile.java
@@ -1,5 +1,24 @@
 package project;
 
 public class GarageProfile {
+	private PricingStrategy pricingStrategy;
+    private double currentDemandLevel = 1.0; // Default level
+
+
+    public void setPricingStrategy(PricingStrategy strategy) {
+        this.pricingStrategy = strategy;
+    }
+
+	public double getCurrentDemandLevel() {
+		return currentDemandLevel;
+	}
+
+	public void setCurrentDemandLevel(double currentDemandLevel) {
+		this.currentDemandLevel = currentDemandLevel;
+	}
+
+	public PricingStrategy getPricingStrategy() {
+		return pricingStrategy;
+	}
 	
 }
