3e500a078974b2d38ea1465f82cdfcd20abfe457
Lybaqadir <lybaqadir53688@gmail.com>
2025-06-24 17:53:19 +0300

    update garageprofile class

--- src/project/GarageProfile.java
+++ src/project/GarageProfile.java
@@ -1,5 +1,8 @@
 package project;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class GarageProfile {
 	public String name;
 	private double currentDemandLevel = 1.0; // Default level
@@ -28,6 +31,25 @@ public class GarageProfile {
 	public String toString() {
 		return "GarageProfile [name=" + name + ", currentDemandLevel=" + currentDemandLevel + "]";
 	}
+/////Added methods for searchService//////
 	
+	private ArrayList<String> availableServiceNames = new ArrayList<>(Arrays.asList("Oil Change", "Tire Replacement", "Diagnostics"));
+	private ArrayList<VehicleType> supportedVehicleTypes = new ArrayList<>(Arrays.asList(VehicleType.CAR, VehicleType.MOTORCYCLE));
+
+	public ArrayList<String> getAvailableServiceNames() {
+	    return availableServiceNames;
+	}
+
+	public ArrayList<VehicleType> getSupportedVehicleTypes() {
+	    return supportedVehicleTypes;
+	}
+	
+	/////Methods for reviews////
+	// ========== Add this to your GarageProfile class ==========
+	private ArrayList<Review> reviews = new ArrayList<>();
+
+	public ArrayList<Review> getReviews() {
+	    return reviews;
+	}
 
 }
